45803ff778b6234d0e1c9568d33cdf330f5ba10f
Phil Hart <phil@Phils-MacBook-Pro.local>
2019-05-29 14:31:01 -0600

    Updated the billing page for the new modal

--- src/components/Billing/BillingPage.js
+++ src/components/Billing/BillingPage.js
@@ -1,20 +1,36 @@
-import React, {Component} from 'react';
-import {Elements, StripeProvider} from 'react-stripe-elements';
-import BillingForm from './BillingForm';
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios'
 
-class BillingPage extends Component {
-  render() {
-    return (
-      <StripeProvider apiKey="pk_test_DxmfbYWW8YHJec5ESRF9n0XS002ib6dHug">
+const URL = 'http://localhost:9000/api/charge'
+
+const successPayment = data => {
+  alert('Payment Successful');
+};
+
+const errorPayment = data => {
+  alert('Payment Error');
+};
+
+export default class BillingPage extends React.Component {
+  onToken = (amount, description) => token =>
+  axios.post(URL,{source: token.id})
+    .then(successPayment)
+    .catch(errorPayment);
+    
+    render() {
+      return (
         <div>
-          <h1>Go Premium Now!</h1>
-          <Elements>
-            <BillingForm />
-          </Elements>
+          <h1>Please Buy Our Premium Things!</h1>
+        <StripeCheckout
+        stripeKey="pk_test_DxmfbYWW8YHJec5ESRF9n0XS002ib6dHug"
+        token={this.onToken}
+        description="For the coolest stuff in the world"
+        amount={1000}
+        currency="USD"
+        name="ChoreMonkey"
+        />
         </div>
-      </StripeProvider>
-    );
+        )
+    }
   }
-}
-
-export default BillingPage;
