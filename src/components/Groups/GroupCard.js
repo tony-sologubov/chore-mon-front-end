0e211fc89343d1c012e0eb28849f110e9c498e56
ryanboris <ryan.boris@gmail.com>
2019-06-06 01:12:06 -0400

    returns vital CRUD functionality to its original and needed location

--- src/components/Groups/GroupCard.js
+++ src/components/Groups/GroupCard.js
@@ -1,15 +1,57 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
-
 import { ComplexButton } from '../Common'
+import FirebaseContext from '../../firebase/context'
 
-const GroupCard = ({ groupName }) => {
-  return (
-    <div>
-      <Link to={`groups/${groupName}`}>
+const GroupCard = ({ groupName, id }) => {
+  const { firebase, user } = useContext(FirebaseContext)
+  const [editedName, setEditedName] = useState('')
+  const [editing, setEditing] = useState(false)
+
+  const group = firebase.firestore.collection('groups').doc(id)
+
+  async function handleEditedNameSubmit(e) {
+    e.preventDefault()
+    console.log(id)
+    firebase.firestore
+      .collection(`users/${user.uid}/groups`)
+      .doc(`${group.id}`)
+      .update({ groupName: editedName })
+  }
+
+  async function deleteGroup() {
+    try {
+      await firebase.firestore
+        .collection(`users/${user.uid}/groups`)
+        .doc(`${group.id}`)
+        .delete()
+    } catch (err) {
+      console.log({ message: err.message, code: err.code })
+    }
+  }
+
+  function toggleEdit() {
+    setEditing(true)
+  }
+
+  return !editing ? (
+    <div className="group-card">
+      <Link to={`groups/${group.id}`}>
         <ComplexButton groupName={groupName} />
       </Link>
+      <button onClick={deleteGroup}>DELETE</button>
+      <button onClick={toggleEdit}>EDIT</button>
     </div>
+  ) : (
+    <form onSubmit={handleEditedNameSubmit}>
+      <input
+        type="text"
+        placeholder={groupName}
+        value={editedName}
+        onChange={e => setEditedName(e.target.value)}
+      />
+      <input type="submit" value="submit" />
+    </form>
   )
 }
 
