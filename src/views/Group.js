d98dc925b40ef414d6d2b8285338705b5e3e49cf
Phil Hart <philhart23@gmail.com>
2019-06-15 09:03:48 -0600

    created modal for edit

--- src/views/Group.js
+++ src/views/Group.js
@@ -10,6 +10,7 @@ import IMAGE3 from '../assets/group-page/png/IMAGE-2.png'
 import IMAGE4 from '../assets/group-page/png/IMAGE-3.png'
 // import Modal from '@material-ui/core/Modal';
 // import Date from '../components/Tasks/Date'
+import Modal from 'react-modal'
 
 
 
@@ -17,8 +18,13 @@ const Group = ({ match }) => {
   const user = JSON.parse(localStorage.getItem('user'))
   const { firebase } = useContext(FirebaseContext)
   const [groupName, setGroupName] = useState('')
+  const [editedName, setEditedName] = useState('')
+  const [editing, setEditing] = useState(false)
+  const [isModalOpen, toggleModal] = useState(false);
   // const [selectedDate, handleDateChange] = useState(new Date());
 // console.log(firebase)
+
+const group = firebase.firestore.collection('groups').doc(match.params.groupId)
 
   const groupRef = firebase.firestore
     .collection(`users/${user.uid}/groups`)
@@ -28,10 +34,66 @@ const Group = ({ match }) => {
     groupRef.get().then(doc => setGroupName(doc.data().groupName))
   }, [groupName, groupRef])
 
+  async function deleteGroup() {
+    try {
+      await firebase.firestore
+        .collection(`users/${user.uid}/groups`)
+        .doc(group.id)
+        .delete()
+    } catch (err) {
+      console.log({ message: err.message, code: err.code })
+    }
+  }
+
+  async function handleEditedNameSubmit(e) {
+    firebase.firestore
+      .collection(`users/${user.uid}/groups`)
+      .doc(group.id)
+      .update({ groupName: editedName })
+  }
+
+  function toggleEdit() {
+    setEditing(true)
+  }
+
+
+
   return (
     <div className="Dashboard">
       <div className="topHeaderAndButtons">
         <h1 className="groupsHeader">{groupName}</h1>
+          <button 
+          className="waves-effect waves-light btn-large  pink hvr-shutter-out-vertical" 
+          onClick={() => toggleModal(!isModalOpen)}>
+          Edit List
+          </button>
+            <Modal isOpen={isModalOpen} toggle={toggleModal}>
+            <form>
+      <input
+      
+        type="text"
+        placeholder={groupName}
+        value={editedName}
+        onChange={e => setEditedName(e.target.value)}
+      />
+
+      <input 
+      className="waves-effect waves-light btn-large  pink hvr-shutter-out-vertical" 
+      type="submit" 
+      value="submit" 
+      onClick={() => { toggleModal(false); handleEditedNameSubmit()}}
+      />
+
+    </form>
+
+      </Modal>
+          <Link to={`/dashboard`}>
+            <button 
+            className="waves-effect waves-light btn-large  pink hvr-shutter-out-vertical" 
+            onClick={deleteGroup}>
+              <span className="iconLinks">Delete List</span>
+            </button>
+          </Link>
         <div className="imageButtons">
           <Link to={`/groups/${match.params.groupId}/add-task`}>
             <button className="threeButtonsOne waves-effect waves-light btn-large pink accent-3 hvr-shutter-out-vertical">
@@ -50,7 +112,7 @@ const Group = ({ match }) => {
               <span className="iconLinks">Dashboard</span>
             </button>
           </Link>
-
+      
         </div>       
             
         </div>
