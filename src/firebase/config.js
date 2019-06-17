69bb54eefd79b1085122d057e13372f6a07a60f3
SabrinaGear <sabrinagear.ft@gmail.com>
2019-06-17 11:03:26 -0500

    fix

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyAf8S7IzLWJXM1UkSU3QAIpnDBZb0wCVgk",
+  authDomain: "chore-monkey-app.firebaseapp.com",
+  databaseURL: "https://chore-monkey-app.firebaseio.com",
+  projectId: "chore-monkey-app",
+  storageBucket: "chore-monkey-app.appspot.com",
+  messagingSenderId: "682615121437",
+  appId: "1:682615121437:web:c546bb1c31d5fdb7"
+};
