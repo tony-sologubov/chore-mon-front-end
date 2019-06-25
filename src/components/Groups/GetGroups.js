36e4098cebcc6250af0b06a40484922ada5442eb
Luisan <luisangelsc@gmail.com>
2019-06-25 09:41:22 -0600

    moved view fetches to Dashboard

--- src/components/Groups/GetGroups.js
+++ src/components/Groups/GetGroups.js
@@ -11,48 +11,11 @@ const groupMembersUrl = "http://localhost:9000/api/groupmembers/"
 
 
 
-const GetGroups = () => {
-  const [groups, setGroups] = useState([])
-  const [groupMembers, setGroupMembers] = useState([])
-
-// Get a list of all the group members a user belongs to
-// Currently hardcoded. Link dynamically once we fix seeds/switch to heroku
-  useEffect(() => {
-    const fetchGroupMembers = async () => {
-      const groupMemberz = await axios
-      .get(`${groupMembersUrl}/user/4`)
-
-      setGroupMembers(groupMemberz.data)
-    }
-    fetchGroupMembers();
-  }, [])
-
-// Set all groups a user belongs to 'groups' so React can render them
-    useEffect(() => {
-      const fetchData = async () => {
-
-        try {
-          //store all the urls for the axios calls in one variable
-          var promises = groupMembers.data.map(group =>
-            `${groupUrl}/${group.groupId}` )
-          //store all axios calls in a variable
-          let allPromises = promises.map(url =>
-            axios.get(url).then(response => response.data))
-          //call and wait until _all_ calls are synced, then store
-          let response = await Promise.all(allPromises)
-          //set groups only once
-          setGroups(response)
-
-        } catch (error) {
-          console.log(error)
-        }
-      }
-    fetchData();
-  }, [groupMembers])
+const GetGroups = ({groups}) => {
 
   if (groups[0]) {
     return groups.map(group => (
-      <GroupCard key={uuidv4()} group={group.data[0]} id={group.id} />
+      <GroupCard key={uuidv4()} group={group} id={group.id} />
       ))
     } else {
       return <h1>Taskings showing up here soon :)</h1>
