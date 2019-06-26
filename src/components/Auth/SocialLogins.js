827f00df975a61e2b721cfbe520c6f8229c38bf2
ryanboris <ryan.boris@gmail.com>
2019-06-25 21:41:47 -0400

    resets social login functionality

--- src/components/Auth/SocialLogins.js
+++ src/components/Auth/SocialLogins.js
@@ -1,51 +1,47 @@
-import React, { useContext } from 'react'
-import { withRouter } from 'react-router-dom'
-import FirebaseContext from '../../firebase/context'
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import { uiConfig } from '../../firebase/uiconfig'
-import firebase from 'firebase'
+import React, { useContext } from 'react';
+import { withRouter } from 'react-router-dom';
+import FirebaseContext from '../../firebase/context';
 
 function SocialLogins({ history }) {
-  const { firebase } = useContext(FirebaseContext)
+  const { firebase } = useContext(FirebaseContext);
 
-  // async function LoginWithGoogle() {
-  //   try {
-  //     await firebase.auth.signInWithPopup(firebase.googleProvider)
-  //   } catch (err) {
-  //     console.error({ message: err.message })
-  //   } finally {
-  //     history.push('/dashboard')
-  //   }
-  // }
+  async function LoginWithGoogle() {
+    try {
+      await firebase.auth.signInWithPopup(firebase.googleProvider);
+    } catch (err) {
+      console.error({ message: err.message });
+    } finally {
+      history.push('/dashboard');
+    }
+  }
 
-  // async function LoginWithFacebook() {
-  //   try {
-  //     await firebase.auth.signInWithPopup(firebase.facebookProvider)
-  //   } catch (err) {
-  //     console.error({ message: err.message })
-  //   } finally {
-  //     history.push('/dashboard')
-  //   }
-  // }
+  async function LoginWithFacebook() {
+    try {
+      await firebase.auth.signInWithPopup(firebase.facebookProvider);
+    } catch (err) {
+      console.error({ message: err.message });
+    } finally {
+      history.push('/dashboard');
+    }
+  }
 
-  // async function LoginWithTwitter() {
-  //   try {
-  //     await firebase.auth.signInWithPopup(firebase.twitterProvider)
-  //   } catch (err) {
-  //     console.error({ message: err.message })
-  //   } finally {
-  //     history.push('/dashboard')
-  //   }
-  // }
+  async function LoginWithTwitter() {
+    try {
+      await firebase.auth.signInWithPopup(firebase.twitterProvider);
+    } catch (err) {
+      console.error({ message: err.message });
+    } finally {
+      history.push('/dashboard');
+    }
+  }
 
   return (
     <div>
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth}/>
-      {/* <button onClick={LoginWithGoogle}>Google</button>
+      <button onClick={LoginWithGoogle}>Google</button>
       <button onClick={LoginWithFacebook}>Facebook</button>
-      <button onClick={LoginWithTwitter}>Twitter</button> */}
+      <button onClick={LoginWithTwitter}>Twitter</button>{' '}
     </div>
-  )
+  );
 }
 
-export default withRouter(SocialLogins)
+export default withRouter(SocialLogins);
