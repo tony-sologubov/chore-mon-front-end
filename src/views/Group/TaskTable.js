c7c2a0c8af96ff52a67ccff15cba083f4a1354b0
SabrinaGear <sabrinagear.ft@gmail.com>
2019-07-01 16:48:49 -0500

    init

--- src/views/Group/TaskTable.js
+++ src/views/Group/TaskTable.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import TinyPic from "../../components/TinyPic";
-
+import EditableLabel from "react-inline-editing";
 class TaskTable extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      title: "",
+      editing: false
+    };
   }
 
   find = id => {
@@ -17,35 +20,59 @@ class TaskTable extends Component {
     }
   };
 
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  edit = () => {
+    return this.setState({ editing: !this.state.editing });
+  };
+
+  submit = () => {};
+
   render() {
     return (
-      <table className="pink striped highlight responsive-table">
-        <thead>
-          <tr>
-            <th>Task</th>
-            <th>Assigned To: </th>
-            <th>Due Date</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
+      <div>
+        <table className="pink striped highlight responsive-table">
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Assigned To: </th>
+              <th>Due Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {this.props.tasks.map(t => {
-            const date = new Date(t.dueDate).toLocaleDateString("en-US");
-            const photo = this.find(t.assignedTo);
-            return (
-              <tr>
-                <td>{t.title}</td>
-                <td>
-                  <TinyPic photo={photo} />
-                </td>
-                <td>{date}</td>
-                <td>Actions</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+          <tbody>
+            {this.props.tasks.map(t => {
+              const date = new Date(t.dueDate).toLocaleDateString("en-US");
+              const photo = this.find(t.assignedTo);
+              return (
+                <tr>
+                  <td>{t.title}</td>
+                  <td>
+                    <TinyPic photo={photo} />
+                  </td>
+                  <td>{date}</td>
+                  <td>
+                    <button onClick={this.edit}>Edit</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        {this.state.editing && (
+          <form onSubmit={this.props.edit}>
+            <input
+              type="text"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleTitleChange}
+            />
+          </form>
+        )}
+      </div>
     );
   }
 }
