dd7a5e7725b76e074912e4ad94f1023b357c78de
Luisan <luisangelsc@gmail.com>
2019-07-03 15:53:14 -0600

    Settings page with Sidebar The sidebar/drawer can be modified to any route/ component we wish. Still needs to be styled

---
+++ src/views/NewSettings.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "../../src/styles/settings.css";
+import Modal from "react-modal";
+import BillingPage from "../components/Billing/BillingPage";
+import Profile from "./Profile";
+import GroupSettings from "./GroupSettings";
+import MiniDrawer from "./MiniDrawer"
+import { Switch, Route, withRouter } from "react-router-dom";
+
+
+
+const user = JSON.parse(localStorage.getItem("user"))
+
+class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+  }
+
+  openModal = () => {
+    this.setState({ showModal: true });
+  };
+
+  closeModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    return (
+      <div className="settingsContainer">
+      <MiniDrawer props={this.props} />
+        <Switch>
+            <Route exact path="/settings/profile" component={Profile} />
+            <Route exact path="/settings/groups" component={GroupSettings} />
+            <Route exact path="/settings/account" component={Profile} />
+            <Route path="/settings/billing" component={BillingPage} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Settings);
